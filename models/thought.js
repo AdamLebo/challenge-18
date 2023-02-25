ddc8303895b4c7582f0d51eb4cba8f78680347bb
AdamLebo <alieberman10@aol.com>
2023-02-24 20:26:38 -0500

    added reaction schema to thought.js

--- models/thought.js
+++ models/thought.js
@@ -28,8 +28,33 @@ const thoughtSchema = new Schema(
     }
   );
 
-
-//   Create a virtual called `reactionCount` that retrieves the length of the thought's `reactions` array field on query.
+  const reactionSchema = new Schema(
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal),
+      },
+    },
+    {
+      toJSON: {
+        getters: true,
+      },
+    }
+  );
   
 thoughtSchema.virtual("reactionCount").get(function () {
     return this.reactions.length;
